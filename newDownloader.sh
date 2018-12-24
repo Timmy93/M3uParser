8a487133af472d16c0dd4d5d315e09ac21f5edfd
Timmy93 <masomeno2006@gmail.com>
2018-12-24 18:29:25 +0100

    Added downloader script

---
+++ newDownloader.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+dir_temp=$2
+dir_finished=$3
+cd $dir_temp
+for i in `seq 1 30`; do
+  timeout 15 curl -O -J -L -C - $1
+  res=$?
+  if test "$res" != "0"; then
+    continue
+  else
+    mv $dir_temp/* $dir_finished
+    exit 1
+  fi
+  echo "Test #$i"
+done
+rm $dir_temp/*
+cd -
+exit 10
