95d72a300cdeb4ed612d4181992be511b35745e8
Timmy93 <federico.seri@mail.polimi.it>
2018-12-24 20:48:56 +0100

    Added class to remember file already uploaded

---
+++ RememberFile.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# Purpose:	
+# Usage:	
+# Author:	Timmy93
+# Date:		
+# Version:	
+# Disclaimer:	
+
+#To implement
+class RememberFiles:
+	
+	def __init__(self):
+		self.files = []
+	
+	
